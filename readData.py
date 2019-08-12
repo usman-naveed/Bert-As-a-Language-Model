6638edd2f982d47cace8e8188e394f64bab95319
M. Usman Naveed <mohammadusman94@hotmail.ca>
2019-08-12 14:34:12 -0400

    testing

--- readData.py
+++ readData.py
@@ -17,7 +17,7 @@ if cwd != "/data/naveed/queensData":
 list_of_DF = []
 for filename in os.listdir(cwd):
     df = pd.read_csv(filename, sep='\t')
-    edited_sentences = df['edited_sentence'].apply(''.join).reset_index()
+    edited_sentences = df['edited_sentence']
     list_of_DF.append(edited_sentences)
 
 print(list_of_DF[1])
