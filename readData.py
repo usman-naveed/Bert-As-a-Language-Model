b7758aaeadfb2bb90cf1c739c7ca1bdcb880580d
M. Usman Naveed <mohammadusman94@hotmail.ca>
2019-08-12 14:18:55 -0400

    testing

--- readData.py
+++ readData.py
@@ -3,22 +3,22 @@ import pandas as pd
 import numpy as np
 
 
-def read_data():
-    #ensure working directory is correct
-    os.chdir("/data/naveed/queensData")
-    cwd = os.getcwd()
-    print(cwd)
 
-    if cwd != "/data/naveed/queensData":
-        print("Not correct directory, current directory of operation is: ", cwd)
-        exit()
+#ensure working directory is correct
+os.chdir("/data/naveed/queensData")
+cwd = os.getcwd()
+print(cwd)
 
-    #loop through all the document in the directory and merge edited_sentence col for each document
-    list_of_DF = np.array()
-    for filename in os.listdir(cwd):
-        df = pd.read_csv(filename, sep='\t')
-        edited_sentences = df['edited_sentence'].apply(''.join).reset_index()
-        list_of_DF.append(edited_sentences)
+if cwd != "/data/naveed/queensData":
+    print("Not correct directory, current directory of operation is: ", cwd)
+    exit()
 
-    return list_of_DF
+#loop through all the document in the directory and merge edited_sentence col for each document
+list_of_DF = np.array()
+for filename in os.listdir(cwd):
+    df = pd.read_csv(filename, sep='\t')
+    edited_sentences = df['edited_sentence'].apply(''.join).reset_index()
+    list_of_DF.append(edited_sentences)
+
+print(list_of_DF[1])
 
