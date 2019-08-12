720a1b0f08e9a30cf57a7e1185dfbff856c91cde
M. Usman Naveed <mohammadusman94@hotmail.ca>
2019-08-12 14:28:19 -0400

    testing

--- readData.py
+++ readData.py
@@ -14,7 +14,7 @@ if cwd != "/data/naveed/queensData":
     exit()
 
 #loop through all the document in the directory and merge edited_sentence col for each document
-list_of_DF = np.array()
+list_of_DF = []
 for filename in os.listdir(cwd):
     df = pd.read_csv(filename, sep='\t')
     edited_sentences = df['edited_sentence'].apply(''.join).reset_index()
