672f00c0439ea8cbc8d0d687cc7f5c0084b8bc03
UsmanNaveed1 <44474805+UsmanNaveed1@users.noreply.github.com>
2019-08-12 18:53:13 -0400

    Update readData.py

--- readData.py
+++ readData.py
@@ -18,7 +18,7 @@ list_of_DF = []
 for filename in os.listdir(cwd):
     df = pd.read_csv(filename, sep='\t')
     edited_sentences = df['edited_sentence'].str.encode(encoding='UTF-8',errors='strict')
-    list_of_DF.append(edited_sentences.str.decode(encoding='UTF-8',errors='strict'))
+    list_of_DF.append(edited_sentences)
 
 
 print(len(list_of_DF))
