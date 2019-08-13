6943cb89c468638aa8684bbffda1d4faf14dfe14
UsmanNaveed1 <44474805+UsmanNaveed1@users.noreply.github.com>
2019-08-12 20:24:51 -0400

    Update readData.py

--- readData.py
+++ readData.py
@@ -15,13 +15,18 @@ if cwd != "/data/naveed/queensData":
 
 #loop through all the document in the directory and merge edited_sentence col for each document
 list_of_DF = []
+merged_strings = []
 for filename in os.listdir(cwd):
     df = pd.read_csv(filename, sep='\t')
     edited_sentences = df['edited_sentence'].str.encode('ascii',errors='ignore')   #supposed to decode to unicode, the encode to ascii, but str.decode function is giving incorrect output. looks like a bug that has already been raised here: https://github.com/pandas-dev/pandas/issues/22613
     list_of_DF.append(edited_sentences)
 
+for document in list_of_DF:
+    merged_strings.append(''.join(document))
 
 print(len(list_of_DF))
+print(len(merged_strings)
 print(list_of_DF[0][0])
+print(merged_strings[0])
 
 
