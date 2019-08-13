df553d5e0aabf2b6044fc4e8e3794b7748be2d53
UsmanNaveed1 <44474805+UsmanNaveed1@users.noreply.github.com>
2019-08-12 22:54:49 -0400

    Update readData.py

--- readData.py
+++ readData.py
@@ -15,6 +15,9 @@ list_of_DF = []
 for filename in os.listdir(cwd):
     df = pd.read_csv(filename, sep='\t')
     edited_sentences = df['edited_sentence'].str.cat(sep=' ')  #https://github.com/pandas-dev/pandas/issues/22613
+    print(edited_sentences)
+    print(type(edited_sentences))
+    exit()
     list_of_DF.append(edited_sentences)
 
 print(len(list_of_DF))
