194f3b2493477bb3b4dad459709488254ba26e90
M. Usman Naveed <mohammadusman94@hotmail.ca>
2019-08-16 12:23:54 -0400

    testing

--- readData.py
+++ readData.py
@@ -20,5 +20,8 @@ def read_data():
         list_of_DF.append(edited_sentences)
 
     print(len(list_of_DF))
-    print(list_of_DF[1].encode('utf-8', errors='ignore'))    #printing just for testing purposes
+    os.chdir("/data/lm/")
+    f = open("edited_sentence_test.txt", "w+")
+    f.write(list_of_DF[1].encode('utf-8', errors= 'ignore'))
+    #print(list_of_DF[1].encode('utf-8', errors='ignore'))    #printing just for testing purposes
     return list_of_DF
