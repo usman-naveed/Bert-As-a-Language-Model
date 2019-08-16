80a3f2b9c9575574ccb30ebabd37d3df80fd1ca6
M. Usman Naveed <mohammadusman94@hotmail.ca>
2019-08-16 12:10:27 -0400

    testing

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import readData
-import run_lm_predict
+#import run_lm_predict
 
 
 # run lm_predict and get the BERT score for each of the documents
 def main():
-    documents = readData.read_data()        #this variable holds the edited_senteces in a list 
+    documents = readData.read_data()        #this variable holds the edited_senteces in a list
 
 
 if __name__ == "__main__":
